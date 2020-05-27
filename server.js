86f89dd5b13b938a86964fde90670b548aa5b183
Muhammad Ali <qtyeert@yahoo.com>
2020-05-27 11:40:25 -0500

    Added Models  - Artical & Comment

--- server.js
+++ server.js
@@ -15,12 +15,7 @@ app.use(
 app.use(express.static(process.cwd() + "/public"));
 
 var exphbs = require("express-handlebars");
-app.engine(
-    "handlebars",
-    exphbs({
-        defaultLayout: "main"
-    })
-);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 const MONGODB_URI =
