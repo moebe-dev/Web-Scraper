ad1046d026c5bc96b78e65d5b2f0a693d28c6b71
Muhammad Ali <qtyeert@yahoo.com>
2020-05-27 11:33:27 -0500

    Added Morgan For Logger, Adeed Port & Made Connection With MongoDB

---
+++ server.js
@@ -0,0 +1,42 @@
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+
+var express = require("express");
+var app = express();
+
+app.use(logger("dev"));
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(express.static(process.cwd() + "/public"));
+
+var exphbs = require("express-handlebars");
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+
+const MONGODB_URI =
+    process.env.MONGODB_URI || "mongodb://localhost/scraper_news";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", function () {
+    console.log("Connected To Mongoose!");
+});
+
+var routes = require("./controller/controller.js");
+app.use("/", routes);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Listening on PORT " + port);
+});
