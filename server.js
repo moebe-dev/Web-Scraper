38b28ae73313317dd72f32df450db4e05a3fe3f7
Muhammad Ali <qtyeert@yahoo.com>
2020-05-28 07:49:10 -0500

    Testing Heroku - Server Does Not Work - Need To Look Into

--- server.js
+++ server.js
@@ -7,25 +7,29 @@ var app = express();
 
 app.use(logger("dev"));
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
+  bodyParser.urlencoded({
+    extended: false
+  })
 );
 
 app.use(express.static(process.cwd() + "/public"));
-
 var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
 app.set("view engine", "handlebars");
 
 const MONGODB_URI =
-    process.env.MONGODB_URI || "mongodb://localhost/scraper_news";
+  process.env.MONGODB_URI || "mongodb://localhost/scraper_news";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 var db = mongoose.connection;
-db.on("error", console.error.bind(console, "Connection Error:"));
+db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
-    console.log("Connected To Mongoose!");
+  console.log("Connected to Mongoose!");
 });
 
 var routes = require("./controller/controller.js");
@@ -33,5 +37,5 @@ app.use("/", routes);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-    console.log("Listening on PORT " + port);
+  console.log("Listening on PORT " + port);
 });
