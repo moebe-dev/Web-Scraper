e01d0294793ff094a145ea906f10127fac440de4
Muhammad Ali <qtyeert@yahoo.com>
2020-05-30 08:39:21 -0500

    Added Clean Files

--- models/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports = {
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost/newsHeadlines",
-  Article: require("./Article"),
-  Comment: require("./Comment"),
-};
